58cccd52b07dcdb1be95aec84b0b651da66f428e
maxime rosa <121794432+vermeil9@users.noreply.github.com>
2023-01-02 11:52:13 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,3 +16,4 @@ pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_mac
 streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 
 # Display the table on the page.
+streamlit.dataframe(my_fruit_list)
